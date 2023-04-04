84042957bcf5d16bf548ae11d61891f3acfa9bf2
Jordan Kay <jordantkay21@gmail.com>
2023-04-04 19:52:47 -0400

    Added SQLExtended Scene

--- Assets/SQLExtendedExample/Scripts/Cube.cs
+++ Assets/SQLExtendedExample/Scripts/Cube.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SqliteExtended : MonoBehaviour
+public class Cube : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
